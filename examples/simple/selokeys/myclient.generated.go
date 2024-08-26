9b42fc00d77830deef1c8c3e4495d6040784f597
kon3gor <bboymishka888@gmail.com>
2024-08-26 18:15:01 +0300

    make simple example actualy simple

--- examples/simple/selokeys/myclient.generated.go
+++
@@ -1,4 +0,0 @@
-// GENERATED USING SELOGEN PLEASE DO NOT EDIT BY HAND
-package selokeys
-
-const MyClient = 0
--- examples/simple/selokeys/myclient2.generated.go
+++
@@ -1,4 +0,0 @@
-// GENERATED USING SELOGEN PLEASE DO NOT EDIT BY HAND
-package selokeys
-
-const MyClient2 = 1
